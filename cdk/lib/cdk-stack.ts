d5aa2d149c2579620d52a93f2bbfa0b26d9cd31a
mmdcloud <mmd26.aka@gmail.com>
2024-09-11 19:23:29 +0530

    updated cdk stack

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,153 +1,164 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as snsSubscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as events from 'aws-cdk-lib/aws-events';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as s3assets from 'aws-cdk-lib/aws-s3-assets';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket, BucketPolicy } from 'aws-cdk-lib/aws-s3';
+import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
+import { Topic, Subscription } from 'aws-cdk-lib/aws-sns';
+import { SnsSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
+import { Role, ServicePrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { Rule, EventPattern } from 'aws-cdk-lib/aws-events';
+import { SnsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { CloudFrontWebDistribution, OriginAccessIdentity, CloudFrontAllowedMethods, CloudFrontAllowedHeaders } from 'aws-cdk-lib/aws-cloudfront';
 
-export class MediaConvertStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class MediaConvertStack extends Stack {
+  constructor(scope: cdk.Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // S3 Buckets
-    const sourceBucket = new s3.Bucket(this, 'ThePlayer007MediaConvertSource', {
+    const sourceBucket = new Bucket(this, 'ThePlayer007MediaConvertSource', {
       bucketName: 'theplayer007-mediaconvert-source',
     });
 
-    const destinationBucket = new s3.Bucket(this, 'ThePlayer007MediaConvertDestination', {
+    const destinationBucket = new Bucket(this, 'ThePlayer007MediaConvertDestination', {
       bucketName: 'theplayer007-mediaconvert-destination',
     });
 
-    // SNS Topic and Subscription
-    const snsTopic = new sns.Topic(this, 'MediaConvertSnsTopic', {
+    // DynamoDB Table
+    const table = new Table(this, 'DynamoDBTable', {
+      tableName: 'MediaconvertRecords',
+      partitionKey: { name: 'RecordId', type: AttributeType.STRING },
+      sortKey: { name: 'Timestamp', type: AttributeType.NUMBER },
+      billingMode: BillingMode.PROVISIONED,
+      readCapacity: 20,
+      writeCapacity: 20,
+    });
+
+    // SNS Topic
+    const snsTopic = new Topic(this, 'MediaConvertSnsTopic', {
       topicName: 'mediaconvert-sns-topic',
     });
 
-    new snsSubscriptions.EmailSubscription(snsTopic.topicArn, {
-      emailAddress: 'mohitfury1997@gmail.com',
-    });
-
-    // EventBridge Rule
-    const rule = new events.Rule(this, 'MediaConvertEventBridgeEvent', {
-      description: 'It monitors the MediaConvert job state change event',
-      eventPattern: {
-        source: ['aws.mediaconvert'],
-        detailType: ['MediaConvert Job State Change'],
-      },
-    });
+    // SNS Subscription
+    // snsTopic.addSubscription(new SnsSubscription({
+    //   endpoint: 'mohitfury1997@gmail.com',
+    //   protocol: sns.SubscriptionProtocol.EMAIL,
+    // }));
 
     // IAM Roles
-    const lambdaExecutionRole = new iam.Role(this, 'MediaConvertLambdaExecutionRole', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-      inlinePolicies: {
-        LambdaCloudwatchPolicy: new iam.PolicyDocument({
-          statements: [
-            new iam.PolicyStatement({
-              actions: [
-                'logs:CreateLogGroup',
-                'logs:CreateLogStream',
-                'logs:PutLogEvents',
-              ],
-              resources: ['*'],
-              effect: iam.Effect.ALLOW,
-            }),
-          ],
-        }),
-        LambdaMediaConvertPolicy: new iam.PolicyDocument({
-          statements: [
-            new iam.PolicyStatement({
-              actions: [
-                'mediaconvert:*',
-                's3:ListAllMyBuckets',
-                's3:ListBucket',
-              ],
-              resources: ['*'],
-              effect: iam.Effect.ALLOW,
-            }),
-          ],
-        }),
-        PassRole: new iam.PolicyDocument({
-          statements: [
-            new iam.PolicyStatement({
-              actions: ['iam:PassRole'],
-              resources: ['*'],
-              effect: iam.Effect.ALLOW,
-              conditions: {
-                StringLike: {
-                  'iam:PassedToService': ['mediaconvert.amazonaws.com'],
-                },
-              },
-            }),
-          ],
-        }),
-      },
+    const lambdaExecutionRole = new Role(this, 'MediaConvertLambdaExecutionRole', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
     });
 
-    const mediaConvertS3Role = new iam.Role(this, 'MediaConvertS3Role', {
-      assumedBy: new iam.ServicePrincipal('mediaconvert.amazonaws.com'),
-      inlinePolicies: {
-        MediaConvertS3Policy: new iam.PolicyDocument({
-          statements: [
-            new iam.PolicyStatement({
-              actions: ['s3:*', 's3-object-lambda:*'],
-              resources: ['*'],
-              effect: iam.Effect.ALLOW,
-            }),
-          ],
-        }),
+    lambdaExecutionRole.addToPolicy(new PolicyStatement({
+      actions: [
+        'logs:CreateLogGroup',
+        'logs:CreateLogStream',
+        'logs:PutLogEvents',
+      ],
+      resources: ['*'],
+    }));
+
+    lambdaExecutionRole.addToPolicy(new PolicyStatement({
+      actions: [
+        'mediaconvert:*',
+        's3:ListAllMyBuckets',
+        's3:ListBucket',
+      ],
+      resources: ['*'],
+    }));
+
+    lambdaExecutionRole.addToPolicy(new PolicyStatement({
+      actions: ['iam:PassRole'],
+      resources: ['*'],
+      conditions: {
+        StringLike: {
+          'iam:PassedToService': ['mediaconvert.amazonaws.com'],
+        },
       },
+    }));
+
+    const mediaConvertS3Role = new Role(this, 'MediaConvertS3Role', {
+      assumedBy: new ServicePrincipal('mediaconvert.amazonaws.com'),
     });
+
+    mediaConvertS3Role.addToPolicy(new PolicyStatement({
+      actions: [
+        's3:*',
+        's3-object-lambda:*',
+      ],
+      resources: ['*'],
+    }));
 
     // Lambda Function
-    const lambdaFunction = new lambda.Function(this, 'MediaConvertFunction', {
-      runtime: lambda.Runtime.PYTHON_3_10,
+    const lambdaFunction = new Function(this, 'MediaConvertFunction', {
       handler: 'lambda_function.lambda_handler',
-      code: lambda.Code.fromAsset('../terraform/lambda_function.zip'),
+      runtime: Runtime.PYTHON_3_10,
+      code: Code.fromAsset('path/to/lambda_function.zip'),
       environment: {
         DestinationBucket: destinationBucket.bucketName,
         MediaConvertRole: mediaConvertS3Role.roleArn,
       },
-      events: [
-        new lambda.EventSourceMapping({
-          eventSourceArn: sourceBucket.bucketArn,
-          target: lambdaFunction,
-          batchSize: 5,
-        }),
-      ],
-      role: lambdaExecutionRole,
     });
 
-    // CloudFront Distribution
-    const oac = new cloudfront.CfnOriginAccessControl(this, 'MediaConvertCloudfrontOriginAccessControl', {
-      originAccessControlConfig: {
-        description: 'mediaconvert-s3-oac',
-        name: 'mediaconvert-s3-oac',
-        originAccessControlOriginType: cloudfront.OriginAccessControlOriginType.S3,
-        signingBehavior: cloudfront.OriginAccessControlSigningBehavior.ALWAYS,
-        signingProtocol: cloudfront.OriginAccessControlSigningProtocol.SIGV4,
+    sourceBucket.grantRead(lambdaFunction);
+
+    // EventBridge Rule
+    new Rule(this, 'MediaConvertEventBridgeEvent', {
+      description: 'It monitors the media convert job state change event',
+      eventPattern: {
+        source: ['aws.mediaconvert'],
+        detailType: ['MediaConvert Job State Change'],
       },
+      targets: [new SnsEventSource(snsTopic)],
     });
 
-    new cloudfront.Distribution(this, 'MediaConvertCloudfrontDistribution', {
-      defaultBehavior: {
-        origin: new cloudfront.origins.S3Origin(destinationBucket, {
-          originAccessControl: oac,
-        }),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
-        cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD,
-        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
-        originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER,
-      },
-      defaultRootObject: 'index.html',
-      domainNames: ['example.com'],
-      priceClass: cloudfront.PriceClass.PRICE_CLASS_200,
-      enabled: true,
-    });
+    // // CloudFront Origin Access Control
+    // const oac = new OriginAccessIdentity(this, 'MediaConvertCloudfrontOriginAccessControl', {
+    //   comment: 'mediaconvert-s3-oac',
+    // });
+
+    // // CloudFront Distribution
+    // new CloudFrontWebDistribution(this, 'MediaConvertCloudfrontDistribution', {
+    //   originConfigs: [{
+    //     s3OriginSource: {
+    //       s3BucketSource: destinationBucket,
+    //       originAccessIdentity: oac,
+    //     },
+    //     behaviors: [{
+    //       isDefaultBehavior: true,
+    //       allowedMethods: CloudFrontAllowedMethods.GET_HEAD,
+    //       allowedHeaders: CloudFrontAllowedHeaders.NONE,
+    //       compress: true,
+    //       defaultTtl: cdk.Duration.seconds(0),
+    //       minTtl: cdk.Duration.seconds(0),
+    //       maxTtl: cdk.Duration.seconds(0),
+    //     }],
+    //   }],
+    //   viewerCertificate: {
+    //     cloudFrontDefaultCertificate: true,
+    //   },
+    //   defaultRootObject: 'index.html',
+    //   priceClass: 'PriceClass_200',
+    // });
+
+    // // Bucket Policy for CloudFront
+    // new BucketPolicy(this, 'MediaConvertDestinationBucketPolicy', {
+    //   bucket: destinationBucket,
+    //   policyDocument: {
+    //     version: '2012-10-17',
+    //     statements: [{
+    //       actions: ['s3:GetObject'],
+    //       resources: [`${destinationBucket.bucketArn}/*`],
+    //       effect: 'Allow',
+    //       principals: [new ServicePrincipal('cloudfront.amazonaws.com')],
+    //       conditions: {
+    //         StringEquals: {
+    //           'AWS:SourceArn': `arn:aws:cloudfront::${cdk.Aws.ACCOUNT_ID}:distribution/${distribution.distributionId}`,
+    //         },
+    //       },
+    //     }],
+    //   },
+    // });
   }
 }
 
